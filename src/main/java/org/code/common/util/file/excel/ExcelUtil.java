2ce6007d8b75081040a301c1ee28cf677c319c2d
yaotianchi <yaotianchi@daojia-inc.com>
2019-10-30 14:29:55 +0800

    优化excel工具类

--- src/main/java/org/code/common/util/file/excel/ExcelUtil.java
+++ src/main/java/org/code/common/util/file/excel/ExcelUtil.java
@@ -2,10 +2,10 @@ package org.code.common.util.file.excel;
 
 import lombok.SneakyThrows;
 import org.apache.poi.hssf.usermodel.*;
+import org.apache.poi.ss.usermodel.HorizontalAlignment;
 import org.code.common.util.file.FileUtil;
 
-import java.io.File;
-import java.io.FileOutputStream;
+import java.io.*;
 
 /**
  * @author yaotianchi
@@ -15,59 +15,56 @@ public class ExcelUtil {
 
 
 
-        /**
-         * 生成一个HSSFWorkbook,是从表格的第一行第一列开始。
-         * @param sheetName sheet名称
-         * @param title 标题
-         * @param startRowIdx 从第几行开始，0表示第一行
-         * @param startColIdx 从第几列开始，0表示第一列
-         * @param values 内容
-         * @param wb HSSFWorkbook对象
-         * @return
-         */
-        public static HSSFWorkbook generateWorkbook(String sheetName,
-                                                    String[] title,
-                                                    int startRowIdx,
-                                                    int startColIdx,
-                                                    String[][] values,
-                                                    HSSFWorkbook wb){
+    /**
+     * 生成一个HSSFWorkbook
+     * @param sheetName sheet名称
+     * @param title 标题
+     * @param startRowIdx 从第几行开始，0表示第一行
+     * @param startColIdx 从第几列开始，0表示第一列
+     * @param values 内容
+     * @param wb HSSFWorkbook对象
+     * @return
+     */
+    public static HSSFWorkbook generateWorkbook(String sheetName,
+                                                String[] title,
+                                                int startRowIdx,
+                                                int startColIdx,
+                                                String[][] values,
+                                                HSSFWorkbook wb) throws Exception{
 
-            // 第一步，创建一个HSSFWorkbook，对应一个Excel文件
-            if(wb == null){
-                wb = new HSSFWorkbook();
-            }
+        if(wb == null){
+            wb = new HSSFWorkbook();
+        }
+        HSSFSheet sheet = wb.createSheet(sheetName);
+        HSSFRow row = sheet.createRow(startRowIdx);
+        HSSFCellStyle style = wb.createCellStyle();
+        style.setAlignment(HorizontalAlignment.CENTER);
+        HSSFFont font = wb.createFont();
+        font.setBold(true);
+        font.setFontHeightInPoints((short)14);
+        style.setFont(font);
+        row.setRowStyle(style); //设置标题样式
+        HSSFCell cell = null;
 
-            // 第二步，在workbook中添加一个sheet,对应Excel文件中的sheet
-            HSSFSheet sheet = wb.createSheet(sheetName);
-
-            // 第三步，在sheet中添加表头第0行,注意老版本poi对Excel的行数列数有限制
-            HSSFRow row = sheet.createRow(startRowIdx);
-
-
-            // 第四步，创建单元格，并设置值表头 设置表头居中
-            HSSFCellStyle style = wb.createCellStyle();
-            style.setAlignment(HSSFCellStyle.ALIGN_CENTER); // 创建一个居中格式
-
-            //声明列对象
-            HSSFCell cell = null;
-
-            //创建标题
-            for(int i=0;i<title.length;i++){
-                cell = row.createCell(i+ startColIdx);
-                cell.setCellValue(title[i]);
-                cell.setCellStyle(style);
-            }
-
-            //创建内容
-            for(int i=0;i<values.length;i++){
-                row = sheet.createRow(i + 1 + startRowIdx);
-                for(int j=0;j<values[i].length;j++){
-                    //将内容按顺序赋给对应的列对象
-                    row.createCell(j + startColIdx).setCellValue(values[i][j]);
+        //创建标题
+        for(int i=0;i<title.length;i++){
+            cell = row.createCell(i+ startColIdx);
+            cell.setCellValue(title[i]);
+            cell.setCellStyle(style);
+        }
+        //创建内容
+        for(int i=0;i<values.length;i++){
+            row = sheet.createRow(i + 1 + startRowIdx);
+            for(int j=0;j<values[i].length;j++){
+                row.createCell(j + startColIdx).setCellValue(values[i][j]);
+                if(i == values.length-1){
+                    sheet.autoSizeColumn((short)(j + startColIdx));
+                    sheet.setColumnWidth(j + startColIdx,sheet.getColumnWidth(j + startColIdx) + 255*5 );
                 }
             }
-            return wb;
         }
+        return wb;
+    }
 
 
         @SneakyThrows
@@ -82,6 +79,21 @@ public class ExcelUtil {
             outputStream.close();
 
         }
+
+    /*
+     * 生成一个HSSFWorkbook的输入流
+     */
+    public static InputStream generateInputStream(String sheetName,
+                                                  String[] title,
+                                                  int startRowIdx,
+                                                  int startColIdx,
+                                                  String[][] values,
+                                                  HSSFWorkbook wb) throws Exception{
+        HSSFWorkbook workbook = generateWorkbook(sheetName, title, startRowIdx, startColIdx, values, wb);
+        ByteArrayOutputStream bos = new ByteArrayOutputStream();
+        workbook.write(bos);
+        return new ByteArrayInputStream(bos.toByteArray());
+    }
 
 
 
